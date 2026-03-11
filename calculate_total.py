8ee93ec43efd4cdb047d58ae712d241f8aabbe29
moresskonyom <tipakolayrambler.ru@mail.ru>
2026-03-11 20:37:58 +0300

    Its definitely completed now
    realized the mistake

--- calculate_total.py
+++ calculate_total.py
@@ -1,11 +1,17 @@
+from decimal import Decimal
+
+
 def calculate_total(prices, *discounts, **options):
-    s = sum(prices)
+    s = Decimal(sum(prices))
     for i in discounts:
-        s *= (100 - i) * 0.01
+        s *= (100 - i) * Decimal(0.01)
     tax = options.get("tax")
     if tax != None:
-        s *= (100 + tax) * 0.01
+        s *= (100 + tax) * Decimal(0.01)
     rd = options.get("round_to")
     if rd != None:
         s = round(s, rd)
-    return s
+    return float(s)
+
+
+print(calculate_total([100, 200, 300], 10, tax=15, round_to=None))
